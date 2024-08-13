bd61089b80cb89c7d40b812e8d7057bf5f2eed9c
shafayetejaman <shafayetejaman@gmail.com>
2024-08-13 11:55:19 +0600

    navbar added

---
+++ text-formater/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function NavBar()
+{
+    return (
+        <heder>
+            <nav className="navbar navbar-expand-lg bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand text-white" href="/">NavBar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarScroll">
+                        <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+                            <li className="nav-item">
+                                <a className="nav-link active text-white" aria-current="page" href="/">Home</a>
+                            </li>
+                        </ul>
+                        {/* <form className="d-flex" role="search">
+                            <input className="form-control me-2 bg-black text-white border-0" type="search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form> */}
+                    </div>
+
+                </div>
+            </nav>
+        </heder>
+    );
+}
